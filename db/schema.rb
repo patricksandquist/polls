2a04c3ead0a84b51156ab82a1b46b98b43bc36cd
Patrick Sandquist <patricksandquist@gmail.com>
2015-09-10 11:37:53 -0700

    add answerchoice

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910182600) do
+ActiveRecord::Schema.define(version: 20150910183142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answer_choices", force: :cascade do |t|
+    t.text    "answer_choice"
+    t.integer "question_id"
+  end
 
   create_table "polls", force: :cascade do |t|
     t.string  "title",     null: false
