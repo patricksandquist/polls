507565f4e002f3e69c539c231f69f6d9a11a836f
Patrick Sandquist <patricksandquist@gmail.com>
2015-09-10 13:54:43 -0700

    add sibling responses

--- app/models/question.rb
+++ app/models/question.rb
@@ -23,4 +23,6 @@ class Question < ActiveRecord::Base
     foreign_key: :poll_id,
     primary_key: :id
   )
+
+  has_many :responses, through: :answer_choices, source: :responses
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.create!(user_name: "Patrick")
+user2 = User.create!(user_name: "Ian")
+poll1 = Poll.create!(title: "Favorite color.", author_id: user1.id)
+question1 = Question.create!(text: "What is your favorite color?", poll_id: poll1.id)
+answer_choice1 = AnswerChoice.create!(answer_choice: "Blue",  question_id: question1.id)
+answer_choice2 = AnswerChoice.create!(answer_choice: "Red",   question_id: question1.id)
+answer_choice3 = AnswerChoice.create!(answer_choice: "Green", question_id: question1.id)
+response1 = Response.create!(user_id: user1.id, answer_choice_id: answer_choice1.id)
+response2 = Response.create!(user_id: user2.id, answer_choice_id: answer_choice3.id)
